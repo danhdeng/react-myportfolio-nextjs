c9accba6f25f2d7328757d4122b7d645124963fb
danhdeng <hui_danny_deng@hotmail.com>
2021-12-11 05:47:28 -0500

    update Header, Hero components

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <>
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Welcome To <br />
+          Dan Deng Personal Portfolio
+        </SectionTitle>
+        <SectionText>
+          I am a seansoned professional software developer, i am interesting
+          exploring all kind of technologies stacks. i have passsion in building
+          web applications in the cloud and sharing my knowledge with others.
+        </SectionText>
+        <Button onClick={props.handleClick}>Learn More</Button>
+      </LeftSection>
+    </Section>
+  </>
 );
 
 export default Hero;
